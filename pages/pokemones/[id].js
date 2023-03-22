904b6f88745e685ed3418cf6dbb6ddf963c4de46
Ruben <rubenmontes330@gmail.com>
2023-03-22 22:05:37 +0000

    Paginas estaticas dinamicas

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,9 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 
 const Pokemon = ({data}) => {
-    console.log(data)
+    const router= useRouter()
+    if(router.isFallback){
+        return <p>Cargando...</p>
+    }
     return (
         <div>
             <h1>{data.name} numero #{data.id}</h1>
@@ -14,9 +18,27 @@ const Pokemon = ({data}) => {
     )
 }
 export default Pokemon
-export const getServerSideProps = async( { params }) => {
+
+export const getStaticProps = async( { params }) => {
     console.log(params);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
     const data = await response.json()
     return {props: {data}}
 }
+
+export const getStaticPaths =  async () => {
+    const paths = [
+        {params: {id: '1'}},
+        {params: {id: '2'}},
+    ]
+    return {
+        paths,
+        fallback: true,
+    }
+}
+// export const getServerSideProps = async( { params }) => {
+//     console.log(params);
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+//     const data = await response.json()
+//     return {props: {data}}
+// }
