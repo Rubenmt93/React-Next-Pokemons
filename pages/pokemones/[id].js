8aa7b1ec163f58c54d4e49c9fabef07567973fc1
Ruben <rubenmontes330@gmail.com>
2023-03-22 21:13:56 +0000

    Agregando server side rendering a una pagina

--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,6 +1,13 @@
-const Pokemon = () => {
+const Pokemon = ({data}) => {
+    console.log(data)
     return (
         <p>lala</p>
     )
 }
 export default Pokemon
+export const getServerSideProps = async( { params }) => {
+    console.log(params);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+    const data = await response.json()
+    return {props: {data}}
+}
